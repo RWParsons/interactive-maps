76731e4da4df2bd7723604f0df628896991d9632
Rex Parsons <47549264+RWParsons@users.noreply.github.com>
2022-08-01 21:51:29 +1000

    reorganise example apps naming convention
    "08-01-name" Chapter 8, example app 1

---
+++ input/apps/03-02-first-leaflet-map/app.R
@@ -0,0 +1,47 @@
+pkgs <- c("shiny", "leaflet", "tidyverse", "sf")
+
+unavailable_pkgs <- c()
+for (pkg in pkgs) {
+  if(!requireNamespace(pkg, quietly = TRUE)) {
+    unavailable_pkgs <- c(unavailable_pkgs, pkg)
+  } else {
+    library(pkg, character.only = TRUE)
+  }
+}
+
+if (length(unavailable_pkgs) != 0) {
+  stop(
+    "The following pkgs are required to run this example\n",
+    "Please run the code below to install them and try again:\n\n",
+    "install.packages(", paste0(paste0("'", unavailable_pkgs, "'"), collapse=", "), ")"
+    )
+}
+
+input_dir <- "../.."
+
+sa2_polygons <- readRDS(file.path(input_dir, "stacked_SA1_and_SA2_polygons_year2016_simplified.rds")) %>%
+  filter(SA_level==2)
+
+ui <- navbarPage(
+  "App-with-a-map", id="nav",
+  tabPanel(
+    "Map",
+    leafletOutput('map')
+  )
+)
+
+server <- function(input, output, session) {
+  output$map <- renderLeaflet({
+    leaflet() %>%
+      addTiles() %>%
+      addPolygons(
+        data=sa2_polygons,
+        fillColor="Orange",
+        color="black",
+        weight=1,
+        group="Polygons"
+      )
+  })
+}
+
+shinyApp(ui, server)
